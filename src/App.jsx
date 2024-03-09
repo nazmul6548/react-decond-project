0dcbebf09eee5d31d2f9e4ba29b58a023c104fcf
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-03-09 22:42:39 +0600

    task

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,10 @@ console.log(num * 2);
 
   return (
     <div>
-      <Explorebydata></Explorebydata>
+      <Explorebydata name="nazmul" age="23"></Explorebydata>
+      <Explorebydata name="islam" age="43"></Explorebydata>
+      <Explorebydata name="jewel" age="32"></Explorebydata>
+      <Explorebydata name="okamma" age="420"></Explorebydata>
         
       
     </div>
--- src/programming shikhbo/exploreByData.jsx
+++ src/programming shikhbo/exploreByData.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+
 const styles = {
-    display : "flex",
+    // display : "flex",
+    textAlign : "center",
     justifyContent : "center",
+    backgroundColor : "gray",
+    width : "400px",
+    padding : "20px",
+    margin : "10px auto",
   
 }
-const Explorebydata = () => {
+const Explorebydata = (props) => {
+    console.log(props.name);
    return (
     <div style={styles}>
 <h1>explor by data</h1>
+<h3>name: {props.name}</h3>
+<h4>Age: {props.age}</h4>
     </div>
    )
 }
