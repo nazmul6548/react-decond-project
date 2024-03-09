a4c485fa7f2a86db164a5ee6e7641cc478726642
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-03-09 21:51:34 +0600

    task

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Explorebydata from "./programming shikhbo/exploreByData";
+
+
+
+function App() {
+
+  const num = (num) =>{
+console.log(num * 2);
+  }
+  
+
+  return (
+    <div>
+      <Explorebydata></Explorebydata>
+        
+      
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/programming shikhbo/exploreByData.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const styles = {
+    display : "flex",
+    justifyContent : "center",
+  
+}
+const Explorebydata = () => {
+   return (
+    <div style={styles}>
+<h1>explor by data</h1>
+    </div>
+   )
+}
+export default Explorebydata;
