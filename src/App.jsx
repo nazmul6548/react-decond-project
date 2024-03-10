c7f89b596218684c08236218b25d81d0d647380a
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-03-10 09:18:49 +0600

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import Explorebydata from "./programming shikhbo/exploreByData";
+// import Explorebydata from "./programming shikhbo/exploreByData";
+import './App.css'
+import ExploreByData from "./programming shikhbo/exploreByData";
+import Hero from './programming shikhbo/hero/Hero';
 
 
 
 function App() {
 
-  const num = (num) =>{
-console.log(num * 2);
-  }
+//   const num = (num) =>{
+// console.log(num * 2);
+//   }
   
 
   return (
     <div>
-      <Explorebydata name="nazmul" age="23"></Explorebydata>
-      <Explorebydata name="islam" age="43"></Explorebydata>
-      <Explorebydata name="jewel" age="32"></Explorebydata>
-      <Explorebydata name="okamma" age="420"></Explorebydata>
-        
+      
+        <ExploreByData></ExploreByData>,
+        <Hero></Hero>
+      
+      
       
     </div>
   );
--- src/programming shikhbo/exploreByData.jsx
+++ src/programming shikhbo/exploreByData.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
 
-const styles = {
-    // display : "flex",
-    textAlign : "center",
-    justifyContent : "center",
-    backgroundColor : "gray",
-    width : "400px",
-    padding : "20px",
-    margin : "10px auto",
+// import React from 'react'
+
+// const styles = {
+//     // display : "flex",
+//     textAlign : "center",
+//     justifyContent : "center",
+//     backgroundColor : "gray",
+//     width : "400px",
+//     padding : "20px",
+//     margin : "10px auto",
   
-}
-const Explorebydata = (props) => {
-    console.log(props.name);
-   return (
-    <div style={styles}>
-<h1>explor by data</h1>
-<h3>name: {props.name}</h3>
-<h4>Age: {props.age}</h4>
+// }
+// const Explorebydata = (props) => {
+//     console.log(props.name);
+//    return (
+//     <div style={styles}>
+// <h1>explor by data</h1>
+// <h3>name: {props.name}</h3>
+// <h4>Age: {props.age}</h4>
+//     </div>
+//    )
+// }
+// export default Explorebydata;
+
+import React from 'react'
+import './explorebydata.css'
+
+const ExploreByData = () => {
+  return (
+    <div className='navcontainer'>
+        {/* <h1>amar desh Bangladesh</h1> */}
+        <a href="#">HOME</a>
+        <a href="#">ABOUT</a>
+        <a href="#">SKILL</a>
+        <a href="#">CONTACT</a>
     </div>
-   )
+  )
 }
-export default Explorebydata;
+
+export default ExploreByData;
